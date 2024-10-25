07165f828f4e11281f84f5448e778822d28ab175
ptchnew <168917171+ptchnew@users.noreply.github.com>
2024-10-25 12:58:56 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+MYIP=$(curl -sS ipv4.icanhazip.com)
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+BIBlue='\033[1;95m'
+BGCOLOR='\e[1;96;101m'
+tyblue='\e[1;36m'
+NC='\e[0m'
+cyann() { echo -e "\\033[36;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+clear
+
+cd /root
+if [ "${EUID}" -ne 0 ]; then
+echo "You need to run this script as root"
+exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+echo "OpenVZ is not supported"
+exit 1
+fi
+
+function ins-tools(){
+cd
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
+clear
+wget https://raw.githubusercontent.com/ptchnew/L2/main/tools.sh &> /dev/null
+chmod +x tools.sh 
+bash tools.sh
+clear
+start=$(date +%s)
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+apt install git curl -y >/dev/null 2>&1
+apt install python -y >/dev/null 2>&1
+}
+
+function ins-package(){
+fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne "  \033[0;35mInstalling Files.. \033[1;37m- \033[0;33m["
+    while true; do
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m#"
+            sleep 0.1s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "\033[0;33m]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne "  \033[0;35mInstalling Files.. \033[1;37m- \033[0;33m["
+    done
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m Succes !\033[1;37m"
+    tput cnorm
+}
+
+
+res3() {
+wget https://raw.githubusercontent.com/ptchnew/L2/main/install/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+clear
+wget https://raw.githubusercontent.com/ptchnew/L2/main/install/insshws.sh && chmod +x insshws.sh && ./insshws.sh
+clear
+}
+
+res4() {
+wget https://raw.githubusercontent.com/ptchnew/L2/main/install/ohp.sh && chmod +x ohp.sh && ./ohp.sh
+clear
+}
+
+res5() {
+wget https://raw.githubusercontent.com/ptchnew/L2/main/update.sh && chmod +x update.sh && ./update.sh
+clear
+}
+
+res6() {
+wget https://raw.githubusercontent.com/ptchnew/L2/main/install/udp-custom.sh && chmod +x udp-custom.sh && bash udp-custom.sh
+clear
+}
+
+echo -e "${BIBlue}╭══════════════════════════════════════════╮${NC}"
+echo -e "${BIBlue}│ ${BGCOLOR}      PROCESS INSTALLED WEBSOCKET SSH   ${NC}${BIBlue} │${NC}"
+echo -e "${BIBlue}╰══════════════════════════════════════════╯${NC}"
+fun_bar 'res3'
+
+echo -e "${BIBlue}╭══════════════════════════════════════════╮${NC}"
+echo -e "${BIBlue}│ ${BGCOLOR}          PROCESS INSTALLED OHP         ${NC}${BIBlue} │${NC}"
+echo -e "${BIBlue}╰══════════════════════════════════════════╯${NC}"
+fun_bar 'res4'
+
+
+echo -e "${BIBlue}╭══════════════════════════════════════════╮${NC}"
+echo -e "${BIBlue}│ ${BGCOLOR}          DOWNLOAD EXTRA MENU           ${NC}${BIBlue} │${NC}"
+echo -e "${BIBlue}╰══════════════════════════════════════════╯${NC}"
+fun_bar 'res5'
+
+echo -e "${BIBlue}╭══════════════════════════════════════════╮${NC}"
+echo -e "${BIBlue}│ ${BGCOLOR}          DOWNLOAD UDP COSTUM           ${NC}${BIBlue} │${NC}"
+echo -e "${BIBlue}╰══════════════════════════════════════════╯${NC}"
+fun_bar 'res6'
+}
+
+ins-tools
+ins-package
+
+cat> /root/.profile << END
+if [ "$BASH" ]; then
+if [ -f ~/.bashrc ]; then
+. ~/.bashrc
+fi
+fi
+mesg n || true
+clear
+menu
+END
+
+chmod 644 /root/.profile
+
+if [ ! -f "/etc/log-create-user.log" ]; then
+echo "Log All Account " > /etc/log-create-user.log
+fi
+history -c
+aureb=$(cat /home/re_otm)
+b=11
+if [ $aureb -gt $b ]
+then
+gg="PM"
+else
+gg="AM"
+fi
+
+cd
+curl -sS ifconfig.me > /etc/myipvps
+curl -s ipinfo.io/city?token=75082b4831f909 >> /etc/xray/city
+curl -s ipinfo.io/org?token=75082b4831f909  | cut -d " " -f 2-10 >> /etc/xray/isp
+rm /root/setup.sh >/dev/null 2>&1
+rm /root/ssh-vpn.sh >/dev/null 2>&1
+rm /root/insshws.sh >/dev/null 2>&1
+rm /root/ohp.sh >/dev/null 2>&1
+rm /root/update.sh >/dev/null 2>&1
+sleep 3
+echo  ""
+cd
+
+rm -rf *
+echo -e "${BIBlue}╭════════════════════════════════════════════╮${NC}"
+echo -e "${BIBlue}│ ${BGCOLOR} INSTALL SCRIPT SELESAI..                 ${NC}${BIBlue} │${NC}"
+echo -e "${BIBlue}╰════════════════════════════════════════════╯${NC}"/
+echo  ""
+sleep 4
+echo -e "[ ${red}WARNING${NC} ] Do you want to reboot now ? (y/n)? "; read answer
+if [ "$answer" == "${answer#[Yy]}" ] ;then
+exit 0
+else
+reboot
+fi
---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/ptchnew/L2/main/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/update "https://raw.githubusercontent.com/ptchnew/L2/main/update.sh" && chmod +x /usr/bin/update
+wget -q -O /usr/bin/addssh "https://raw.githubusercontent.com/ptchnew/L2/main/menu/addssh.sh" && chmod +x /usr/bin/addssh
+wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/ptchnew/L2/main/menu/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
+wget -q -O /usr/bin/delssh "https://raw.githubusercontent.com/ptchnew/L2/main/menu/delssh.sh" && chmod +x /usr/bin/delssh
+wget -q -O /usr/bin/limitssh "https://raw.githubusercontent.com/ptchnew/L2/main/menu/limitssh.sh" && chmod +x /usr/bin/limitssh
+wget -q -O /usr/bin/lock "https://raw.githubusercontent.com/ptchnew/L2/main/menu/lock.sh" && chmod +x /usr/bin/lock
+wget -q -O /usr/bin/renewssh "https://raw.githubusercontent.com/ptchnew/L2/main/menu/renewssh.sh" && chmod +x /usr/bin/renewssh
+wget -q -O /usr/bin/unlock "https://raw.githubusercontent.com/ptchnew/L2/main/menu/unlock.sh" && chmod +x /usr/bin/unlock
+wget -q -O /usr/bin/speedtest "https://raw.githubusercontent.com/ptchnew/L2/main/install/speedtest_cli.py" && chmod +x /usr/bin/speedtest
+wget -q -O /usr/bin/tendang "https://raw.githubusercontent.com/ptchnew/L2/main/menu/tendang.sh" && chmod +x /usr/bin/tendang
+wget -q -O /usr/bin/bantwidth "https://raw.githubusercontent.com/ptchnew/L2/main/install/bantwidth" && chmod +x /usr/bin/bantwidth
+wget -q -O /usr/bin/xp "https://raw.githubusercontent.com/ptchnew/L2/main/install/xp.sh" && chmod +x /usr/bin/xp
+sleep 1
+exit
